0260eb4a43fff1fec84435afbaef7fa7eaf0b0b7
ShakunBoris <shakun.boris@gmail.com>
2022-12-29 17:54:24 +0400

    application model node done yet

--- colorizer/colorizer/settings.py
+++ colorizer/colorizer/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'front.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- colorizer/front/models.py
+++ colorizer/front/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+class Application(models.Model):
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(max_length=254)
+    # country = 
+    # whatsapp
+    # status 
