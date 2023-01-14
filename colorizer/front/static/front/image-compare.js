a6e217d5c076da08efed910bb2a5a1d8374f9712
ShakunBoris <shakun.boris@gmail.com>
2023-01-14 20:44:41 +0400

    relatively done  responsible img compare w/ MARGIN

--- colorizer/front/static/front/image-compare.js
+++ colorizer/front/static/front/image-compare.js
@@ -1,4 +1,4 @@
-// alert('!!!');
+
 document.addEventListener("DOMContentLoaded", () => {
   const element = document.getElementById("image-compare");
   const viewer = new ImageCompare(element).mount();
