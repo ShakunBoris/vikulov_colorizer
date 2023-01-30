c71117f09718d0e6299e9936d8b737558da87411
ShakunBoris <shakun.boris@gmail.com>
2023-01-30 17:02:33 +0400

    if debut static loaded by django othewise not

--- colorizer/colorizer/urls.py
+++ colorizer/colorizer/urls.py
@@ -21,4 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('front.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
