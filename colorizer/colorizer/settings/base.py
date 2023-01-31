7d9a3eed1b93ebd09606accf17aaca89617f9df5
ShakunBoris <shakun.boris@gmail.com>
2023-01-31 22:54:47 +0400

    cors headers installed wtf

--- colorizer/colorizer/settings/base.py
+++ colorizer/colorizer/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ] 
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'colorizer.urls'
@@ -124,4 +126,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['https://vikulovcolorizer-production.up.railway.app/contact']
+CSRF_TRUSTED_ORIGINS = ['https://vikulovcolorizer-production.up.railway.app/contact', 'https://*.railway.app']
