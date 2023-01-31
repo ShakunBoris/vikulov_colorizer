b8b02aebfd94d06369d8be7ce76b56b5c9d392a7
ShakunBoris <shakun.boris@gmail.com>
2023-01-31 20:53:08 +0400

    dev.py debug changed and allowed hosts

--- colorizer/colorizer/settings/dev.py
+++ colorizer/colorizer/settings/dev.py
@@ -3,6 +3,6 @@ from .base import *
 SECRET_KEY = 'django-insecure-^i&*nk$7$5&6-wt713-dyexy_lq(9m_z2+g3@7g2kabmv3u&uk'
 print(SECRET_KEY[:round(len(SECRET_KEY)/3)])
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
