1fb57693eeb964ce1864512524da7e61901ae695
ShakunBoris <shakun.boris@gmail.com>
2023-01-31 21:40:30 +0400

    no secret key print

--- colorizer/colorizer/settings/dev.py
+++ colorizer/colorizer/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 SECRET_KEY = 'django-insecure-^i&*nk$7$5&6-wt713-dyexy_lq(9m_z2+g3@7g2kabmv3u&uk'
-print(SECRET_KEY[:round(len(SECRET_KEY)/3)])
+# print(SECRET_KEY[:round(len(SECRET_KEY)/3)])
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- colorizer/colorizer/settings/prod.py
+++ colorizer/colorizer/settings/prod.py
@@ -2,8 +2,8 @@ from .base import *
 import os
 
 SECRET_KEY = os.environ['SECRET_KEY']
-print(os.environ['SECRET_KEY'])
-print('!!!!!!!!!!!!!!!!!!!!!!!!! PROD.Py!!!!!!!!!!!!!!!!!!!!!!')
+# print(os.environ['SECRET_KEY'])
+# print('!!!!!!!!!!!!!!!!!!!!!!!!! PROD.Py!!!!!!!!!!!!!!!!!!!!!!')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ['*']
