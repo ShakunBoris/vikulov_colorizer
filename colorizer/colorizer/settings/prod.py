b9797211ec2ec3e68e9ab6dc04c218a61cc155a9
ShakunBoris <shakun.boris@gmail.com>
2023-01-31 20:35:44 +0400

    to railway againg

--- colorizer/colorizer/settings/prod.py
+++ colorizer/colorizer/settings/prod.py
@@ -1,7 +1,9 @@
 from .base import *
+import os
 
-SECRET_KEY = 'django-insecure-^i&*nk$7$5&6-wt713-dyexy_lq(9m_z2+g3@7g2kabmv3u&uk'
+SECRET_KEY = os.environ('SECRET_KEY')
+print(os.environ('SECRET_KEY'))
 print('!!!!!!!!!!!!!!!!!!!!!!!!! PROD.Py!!!!!!!!!!!!!!!!!!!!!!')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
