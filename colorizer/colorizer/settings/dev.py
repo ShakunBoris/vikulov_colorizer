59eaac7d089731647ad9662a50c4dd74bf6ff1b7
ShakunBoris <shakun.boris@gmail.com>
2023-01-31 22:48:08 +0400

    csrf

--- colorizer/colorizer/settings/dev.py
+++ colorizer/colorizer/settings/dev.py
@@ -6,4 +6,4 @@ SECRET_KEY = 'django-insecure-^i&*nk$7$5&6-wt713-dyexy_lq(9m_z2+g3@7g2kabmv3u&uk
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '*',]
-CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+
