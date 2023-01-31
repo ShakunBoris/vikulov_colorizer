5fbffc0f1c988588c8e8cd00d4725881c63c81ba
ShakunBoris <shakun.boris@gmail.com>
2023-01-31 22:32:31 +0400

    works! back to debug

--- colorizer/colorizer/urls.py
+++ colorizer/colorizer/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ] 
 # if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # else:
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
