1e64c20c5cf11d818984699cc4ad7abf0f68b0c1
ShakunBoris <shakun.boris@gmail.com>
2023-01-31 22:56:55 +0400

    'corsheaders.middleware.CorsMiddleware', moved up

--- colorizer/colorizer/settings/base.py
+++ colorizer/colorizer/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ] 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'colorizer.urls'
