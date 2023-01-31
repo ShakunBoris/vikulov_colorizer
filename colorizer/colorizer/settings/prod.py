339ae357ef0530e042b311219212c49b1a7bc939
ShakunBoris <shakun.boris@gmail.com>
2023-01-31 20:38:49 +0400

    try

--- colorizer/colorizer/settings/prod.py
+++ colorizer/colorizer/settings/prod.py
@@ -1,8 +1,8 @@
 from .base import *
 import os
 
-SECRET_KEY = os.environ('SECRET_KEY')
-print(os.environ('SECRET_KEY'))
+SECRET_KEY = os.environ['SECRET_KEY']
+print(os.environ['SECRET_KEY'])
 print('!!!!!!!!!!!!!!!!!!!!!!!!! PROD.Py!!!!!!!!!!!!!!!!!!!!!!')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
