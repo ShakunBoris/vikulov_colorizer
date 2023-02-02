2e8bf0046bb063657a8e8f4ab92f42f5ee1ed90a
ShakunBoris <shakun.boris@gmail.com>
2023-02-02 14:11:29 +0400

    debug 1

--- colorizer/colorizer/settings/dev.py
+++ colorizer/colorizer/settings/dev.py
@@ -3,7 +3,7 @@ from .base import *
 SECRET_KEY = 'django-insecure-^i&*nk$7$5&6-wt713-dyexy_lq(9m_z2+g3@7g2kabmv3u&uk'
 # print(SECRET_KEY[:round(len(SECRET_KEY)/3)])
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'vikulovcolorizer-production.up.railway.app']
 # ALLOWED_HOSTS = ['*']
