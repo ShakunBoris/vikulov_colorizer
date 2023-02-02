175a08111fc51bf00cfe80140ad8b3b48b86ebc9
ShakunBoris <shakun.boris@gmail.com>
2023-02-02 12:17:42 +0400

    settings mimetypes.add for admin styles

--- colorizer/colorizer/settings/base.py
+++ colorizer/colorizer/settings/base.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+# FOR ADMIN STYLES TO WORK????
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
