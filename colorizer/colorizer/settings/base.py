150f8c7a6bfa2a0054ae08fdb8cb9c1c8012193d
ShakunBoris <shakun.boris@gmail.com>
2023-02-02 13:03:15 +0400

    mimetypes.add_type("text/html", ".html"

--- colorizer/colorizer/settings/base.py
+++ colorizer/colorizer/settings/base.py
@@ -16,6 +16,7 @@ import os
 # FOR ADMIN STYLES TO WORK????
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".html", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
