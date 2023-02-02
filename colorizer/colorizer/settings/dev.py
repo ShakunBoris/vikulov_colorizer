7d85451cf497e4645c9400a8501ea955a2171e8e
ShakunBoris <shakun.boris@gmail.com>
2023-02-02 12:53:00 +0400

    ALLOWED_HOSTS = ['vikulovcoloriz.up.railway.app']

--- colorizer/colorizer/settings/dev.py
+++ colorizer/colorizer/settings/dev.py
@@ -5,6 +5,6 @@ SECRET_KEY = 'django-insecure-^i&*nk$7$5&6-wt713-dyexy_lq(9m_z2+g3@7g2kabmv3u&uk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['vikulovcolorizer-production.up.railway.app']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['vikulovcolorizer-production.up.railway.app']
+# ALLOWED_HOSTS = ['*']
 
