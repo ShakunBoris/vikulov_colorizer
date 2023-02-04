39b93a0eaab92dc7b0fb1f4dd0df8706762bb860
ShakunBoris <shakun.boris@gmail.com>
2023-02-04 16:35:34 +0400

    #RAILWAY TEST MANUAL CONNECT POSTGRES

--- colorizer/colorizer/settings/dev.py
+++ colorizer/colorizer/settings/dev.py
@@ -8,13 +8,35 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'colorizer.up.railway.app']
 # ALLOWED_HOSTS = ['*']
 
+
+#local POSTGRES
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'psql_test',
+#        'USER': 'postgres',
+#        'PASSWORD': 'postgres',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+# }
+
+#RAILWAY TEST MANUAL CONNECT POSTGRES
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'psql_test',
+       'NAME': 'railway',
        'USER': 'postgres',
-       'PASSWORD': 'postgres',
-       'HOST': '127.0.0.1',
-       'PORT': '5432',
+       'PASSWORD': 'o8FbMYYcKAPgKYKEJIzc',
+       'HOST': 'containers-us-west-146.railway.app',
+       'PORT': '7010',
    }
 }
+
+# LOCAL SQLITE
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
